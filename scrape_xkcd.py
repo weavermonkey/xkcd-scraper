1caf356e7248a98215da9ab829c6ad2263ee12d8
weavermonkey <vembarvarun@gmail.com>
2017-11-01 17:39:04 +0530

    fix comic numbering bug

--- scrape_xkcd.py
+++ scrape_xkcd.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import time
 import requests
+import os
+import urllib
+
 
 initial_url = 'https://xkcd.com/'
 current_url = ''
@@ -14,11 +17,14 @@ while(page_request.status_code != 404):
 		current_page = requests.get(current_url)
 	html_content = current_page.text
 	soup_obj = BeautifulSoup(html_content,'lxml')
-	comic_class_div = soup_obj.find('div',{'id':'comic'})
-	curr_comic_title = comic_class_div.img['title']
-	comic_nav_div = soup_obj.find('ul',{'class':'comicNav'})
-	next_comic_url = comic_nav_div.find_all('a')[1]['href'].replace('/','')
-	print curr_comic_title, next_comic_url
+	curr_comic_title = soup_obj.find('div',{'id':'comic'}).img['title']
+	curr_image_url = 'http:'+ soup_obj.find('div',{'id':'comic'}).img['src']
+	next_comic_url = soup_obj.find('ul',{'class':'comicNav'}).find_all('a')[1]['href'].replace('/','')
+	print curr_comic_title, next_comic_url,curr_image_url
 	current_url = initial_url + next_comic_url
+	os.mkdir(str(int(next_comic_url)+1))
+	alt_text_file = open( ( os.path.join( os.getcwd(), str(int(next_comic_url)+1) ) ) + '/' + str(int(next_comic_url)+1) + '.txt', 'w' )
+	alt_text_file.write(curr_comic_title)
+	alt_text_file.close()
+	urllib.urlretrieve( curr_image_url, os.path.join(os.getcwd(), str(int(next_comic_url)+1)) + '/' +str(int(next_comic_url)+1)+'.png' )
 	time.sleep(0.8)
-	print '###################################################'
