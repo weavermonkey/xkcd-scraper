892d919f3129d70f33df6db615ccbbbaf12293dc
weavermonkey <vembarvarun@gmail.com>
2017-11-02 09:18:37 +0530

    add requirements.txt

--- scrape_xkcd.py
+++ scrape_xkcd.py
@@ -4,7 +4,6 @@ import requests
 import os
 import urllib
 
-
 initial_url = 'https://xkcd.com/'
 current_url = ''
 
@@ -20,12 +19,11 @@ while( page_request.status_code != 404 ):
 	curr_comic_title = soup_obj.find( 'div', {'id':'comic'} ).img['title']
 	curr_image_url = 'http:'+ soup_obj.find( 'div',{'id':'comic'} ).img['src']
 	next_comic_url = soup_obj.find( 'ul',{'class':'comicNav'} ).find_all('a')[1]['href'].replace( '/', '' )
-	print curr_comic_title, next_comic_url,curr_image_url
 	current_url = initial_url + next_comic_url
 	comic_path_on_local = str( int( next_comic_url ) + 1 )
 	os.mkdir( comic_path_on_local )
 	alt_text_file = open( ( os.path.join( os.getcwd(), comic_path_on_local ) ) + '/' + comic_path_on_local + '.txt', 'w' )
-	alt_text_file.write(curr_comic_title)
+	alt_text_file.write( curr_comic_title )
 	alt_text_file.close()
 	urllib.urlretrieve( curr_image_url, os.path.join( os.getcwd(), comic_path_on_local ) + '/' + comic_path_on_local +'.png' )
-	time.sleep(0.8)
+	time.sleep( 0.8 )
