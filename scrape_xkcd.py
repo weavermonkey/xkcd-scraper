b045df5794185ed67bbb96b351686d909f4c3d90
weavermonkey <vembarvarun@gmail.com>
2017-11-06 17:24:35 +0530

    fix unicode error

--- scrape_xkcd.py
+++ scrape_xkcd.py
@@ -1,29 +1,29 @@
-from bs4 import BeautifulSoup
-import time
 import requests
+from bs4 import BeautifulSoup
 import os
 import urllib
+import time
 
 initial_url = 'https://xkcd.com/'
-current_url = ''
+next_url = ''
 
 page_request = requests.get( initial_url )
 
 while( page_request.status_code != 404 ):
-	if current_url == '':
+	if next_url == '':
 		current_page = requests.get( initial_url )
 	else:
-		current_page = requests.get( current_url )
+		current_page = requests.get( next_url )
 	html_content = current_page.text
 	soup_obj = BeautifulSoup( html_content,'lxml' )
-	curr_comic_title = soup_obj.find( 'div', {'id':'comic'} ).img['title']
-	curr_image_url = 'http:'+ soup_obj.find( 'div',{'id':'comic'} ).img['src']
-	next_comic_url = soup_obj.find( 'ul',{'class':'comicNav'} ).find_all('a')[1]['href'].replace( '/', '' )
-	current_url = initial_url + next_comic_url
-	comic_path_on_local = str( int( next_comic_url ) + 1 )
-	os.mkdir( comic_path_on_local )
-	alt_text_file = open( ( os.path.join( os.getcwd(), comic_path_on_local ) ) + '/' + comic_path_on_local + '.txt', 'w' )
-	alt_text_file.write( curr_comic_title )
+	alt_text = soup_obj.find( 'div', {'id':'comic'} ).img['title'].encode('ascii','ignore').decode()
+	comic_image_url = 'http:'+ soup_obj.find( 'div',{'id':'comic'} ).img['src']
+	next_comic_number = soup_obj.find( 'ul',{'class':'comicNav'} ).find_all('a')[1]['href'].replace( '/', '' )
+	comic_path_on_disk = str( int( next_comic_number ) + 1 )
+	os.mkdir( comic_path_on_disk )
+	alt_text_file = open( ( os.path.join( os.getcwd(), comic_path_on_disk ) ) + '/' + comic_path_on_disk + '.txt', 'w' )
+	alt_text_file.write( alt_text )
 	alt_text_file.close()
-	urllib.urlretrieve( curr_image_url, os.path.join( os.getcwd(), comic_path_on_local ) + '/' + comic_path_on_local +'.png' )
+	urllib.urlretrieve( comic_image_url, os.path.join( os.getcwd(), comic_path_on_disk ) + '/' + comic_path_on_disk +'.png' )
+	next_url = initial_url + next_comic_number
 	time.sleep( 0.8 )
